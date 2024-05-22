4832aedccda76f530ebde647e87beb5edfde4340
harshkashyap <ece.harshkashyap1@gmail.com>
2024-05-22 10:15:43 +0530

    change css

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import './App.css'
 import Section1 from './Components/Section1'
 import MagicCursor from './Components/MagicCursor'
 
+
 function App() {
   return (
     <>
+      
     <MagicCursor/>
     <Section1/>
     </>
